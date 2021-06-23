38eb33164be39b484e2b20bae31def8ef2757aec
Anastasiia Sanatar <sanatar.nastya@gmail.com>
2021-06-23 23:53:58 +0300

    пофиксила  resolve

--- js/task3.js
+++ js/task3.js
@@ -13,8 +13,8 @@ const makeTransaction = (transaction => {
         setTimeout(() => {
             const canProcess = Math.random() > 0.3;
 
-            if (canProcess) {
-            resolve(transaction.id, delay);
+          if (canProcess) {
+            resolve([transaction.id, delay]);
             } else {
             reject(transaction.id);
             }
@@ -23,7 +23,7 @@ const makeTransaction = (transaction => {
 })
 
 
-const logSuccess = (id, time) => {
+const logSuccess = ([id, time]) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
